bcbf90088491b0053a4f28289af8de5bffb306bf
todayis_yok <yog2546@gmail.com>
2025-01-25 17:09:36 +0700

    lab8_038

---
+++ lib/pages/home.dart
@@ -0,0 +1,35 @@
+import 'package:auth_buttons/auth_buttons.dart';
+import 'package:flutter/material.dart';
+import 'package:lab8_038/services/auth_service.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  AuthService authService = AuthService();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("login"),
+      ),
+      body: Center(
+        child: Container(
+          child: GoogleAuthButton(
+            onPressed: () {
+              authService.signInWithGoogle().then(
+                (value) {
+                  print(value);
+                },
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
